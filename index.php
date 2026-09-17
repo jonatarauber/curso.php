42584404d70ae756506bd78fb361dcedd11f0876
jonatarauber <jonatarauber85@gmail.com>
2026-09-17 12:26:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro teste de PHP</title>
+    <style>
+        h2 {
+            color: red;
+            text-shadow: 1px 1px 2px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <h2>
+    <?php
+        echo "Olá,<br> mundo!";
+        ?>
+    </h2>
+</body>
+</html>
