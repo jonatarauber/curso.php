06ea7e4e5650b9534940ff172ad33865a81a13f5
jonatarauber <jonatarauber85@gmail.com>
2026-09-18 10:01:45 -0300

    aula04

---
+++ aula04.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title></title>
+</head>
+<body>
+<div>
+    <?php
+     $idade = 18;
+     $nome = "Maria";
+     $n = 4.5;
+     echo "$nome tem $idade  anos!";
+    ?>
+</div>
+</body>
+</html>
